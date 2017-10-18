596eb496f6711a637fc39d312af3defde50367e1
mkim1994 <mkim2@andrew.cmu.edu>
2017-10-18 14:34:25 -0400

    project setup

---
+++ Assets/Scripts/Util/MaterialDB.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Material DB")]
+public class MaterialDB : ScriptableObject {
+    [SerializeField]
+    private Material[] tileMats;
+    public Material[] TileMats { get { return tileMats; } }
+
+
+  /*  [SerializeField]
+    private Material tileMatY;
+    public Material TileMatY { get { return tileMatY; }}
+
+	[SerializeField]
+	private Material tileMatB;
+	public Material TileMatB { get { return tileMatB; } }
+
+	[SerializeField]
+	private Material tileMatG;
+	public Material TileMatG { get { return tileMatG; } }
+
+	[SerializeField]
+	private Material tileMatR;
+	public Material TileMatR { get { return tileMatR; } }*/
+
+    [SerializeField]
+    private Material[] boardMats;
+    public Material[] BoardMats { get { return boardMats; }}
+
+}
