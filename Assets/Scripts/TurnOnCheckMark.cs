768513181d34c054166aba7addecf30dec01ed33
Unknown <dmf463@nyu.edu>
2017-12-03 16:49:04 -0500

    made so the content stays when you click

---
+++ Assets/Scripts/TurnOnCheckMark.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnCheckMark : MonoBehaviour {
+
+    public GameObject checkMark;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CheckMark()
+    {
+        checkMark.SetActive(true);
+    }
+}
